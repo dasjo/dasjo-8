7d2611ad58c3ffa74743d2d7c0678e858915cd31
dasjo <dasjogibtsnet@gmail.com>
2014-01-14 00:06:22 +0100

    Added contrib modules field_group and honeypot.

---
+++ modules/field_group/lib/Drupal/field_group/FieldGroupAddGroup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\field_group\FieldGroupAddGroup.
+ */
+
+namespace Drupal\field_group;
+
+use Drupal\field_group\FieldGroup;
+
+/**
+ * Provides an interface defining a field_group data object.
+ */
+class FieldGroupAddGroup {
+
+
+}
